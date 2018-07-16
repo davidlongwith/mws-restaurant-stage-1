1959604529f195dcff50c9bc49789322e74236bc
davidlongwith <david@davidlongwith.com>
2018-07-16 19:35:29 -0400

    add scope

--- register_sw.js
+++ register_sw.js
@@ -1,7 +1,9 @@
 
 /* Register Service Worker */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(function() {
+  navigator.serviceWorker.register('/sw.js', {
+    scope: '/'
+  }).then(function() {
     console.log('Yay! Service Worker Registered!');
   }).catch(function() {
     console.log('Service Worker Registration Failed');
