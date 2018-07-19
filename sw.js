1429721ebcd90782a5c336e44afd436926f54007
davidlongwith <david@davidlongwith.com>
2018-07-19 13:15:35 -0400

    sw - minor code updates

--- sw.js
+++ sw.js
@@ -2,34 +2,36 @@
 
 
 var cacheID = "restaurant-reviews-v1";
+var cacheURLs = [
+      '/',
+      '/index.html',
+      '/restaurant.html',
+      '/css/styles.css',
+      '/js/dbhelper.js',
+      '/js/main.js',
+      '/js/restaurant_info.js',
+      '/register_sw.js',
+      '/sw.js',
+      '/data/restaurants.json'
+    ]
 
+/* on service worker install event add items to cache */
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheID).then(function(cache) {
       console.log('cache opened!');
-      return cache.addAll(
-        [
-          '/',
-          '/index.html',
-          '/restaurant.html',
-          '/css/styles.css',
-          '/js/dbhelper.js',
-          '/js/main.js',
-          '/js/restaurant_info.js',
-          '/register_sw.js',
-          '/sw.js',
-          '/data/restaurants.json'
-        ]
-      );
+      return cache.addAll(cacheURLs);
     })
   );
 });
 
+/* fetch the network request */
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
         if (response) {    /* if match in cache return that */
+          console.log(response.url);
           return response;
         }
         return fetch(event.request);    /* or just return network request as usual */
