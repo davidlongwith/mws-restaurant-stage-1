9cf29ab5ef6cfc7aa921811d69da5c68b7f08224
davidlongwith <david@davidlongwith.com>
2018-07-22 15:02:28 -0400

    improve image alt tags

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.name;
+  image.alt = 'Image for the restaurant ' + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,7 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
-  image.alt = restaurant.name;
+  image.alt = 'Image for the restaurant ' + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
